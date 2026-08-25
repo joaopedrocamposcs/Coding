7f7cd21648f0ce15249c99be18c7039d4b381b2a
João Pedro Campos dos Santos <joaopedro.campos.cs@gmail.com>
2026-08-25 20:55:34 -0300

    atividade
    resolvendo atividades

---
+++ programação/Lista 01 João Pedro.py
@@ -0,0 +1,32 @@
+#exercício 1
+
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"Olá, {nome}! Você tem {idade} anos!")
+
+#exercicio 2
+
+um = int(input("Digite o primeiro numero: "))
+dois = int(input("Digite o segundo numero: "))
+
+print(f"Soma = {um + dois}")
+
+#exercicio 3
+
+numero = int(input("Digite um número: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("Antecessor:", antecessor)
+print("Sucessor:", sucessor)
+
+
+#exercicio 4
+
+num = int(input("Digite um numero: "))
+
+print(f" O dobro é {num * 2}")
+
+#exercicio 5 
